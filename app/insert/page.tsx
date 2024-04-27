c7e12c4151a5454becabb95b7a5134e0131f9198
Vynicius <92867151+VyniciusCorrea@users.noreply.github.com>
2024-04-26 22:32:30 -0300

    feat:teste

---
+++ app/insert/page.tsx
@@ -0,0 +1,15 @@
+import { Input } from "@/components/ui/input";
+
+const Insert = () => {
+    return ( 
+        <><div>
+         
+        </div>
+        <div className="mt-8">
+                <Input className="w-96" type="text" placeholder="Pesquisa"/>
+        </div></>
+        
+     );
+}
+ 
+export default Insert;
